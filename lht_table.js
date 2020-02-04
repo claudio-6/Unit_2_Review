7f63d8fe653f382bcbbb070474dd334b60c8fb0b
claudio <crios235@west-mec.org>
2020-02-04 16:48:08 -0700

    finishing touches

--- lht_table.js
+++ lht_table.js
@@ -12,8 +12,8 @@
 */
 //GLOBAL VARIABLES 
 var thisDay = new Date("August 30, 2018"); 
-var tableHTML = "<table id='evenList'><caption>upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>";
-var endDate = new Date( thisDay.getTime() + 14*24*60*1000);
+var tableHTML = "<table id='eventTable'><caption>upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+var endDate = new Date( thisDay.getTime() + 14*24*60*60*1000);
 //loop for calendar
 for (var i = 0; i < eventDates.length; i++){
    var eventDate = new Date(eventDates[i]);
@@ -23,11 +23,12 @@ for (var i = 0; i < eventDates.length; i++){
    if(thisDay <= eventDate && eventDate <= endDate){
       tableHTML += "<tr>";
       tableHTML += "<td>" + eventDay + "@" + eventTime + "</td>";
-      tableHTML += "<td>" + description + "<td>"
-      tableHTML += "<td>" + eventPrices + "</td>";
+      tableHTML += "<td>" + eventDescriptions[i] + "</td>";
+      tableHTML += "<td>" + eventPrices[i] + "</td>";
       tableHTML += "</tr>";
    }
-
 }
 
+tableHTML += "</table>";
+document.getElementById("eventList").innerHTML = tableHTML;
 
